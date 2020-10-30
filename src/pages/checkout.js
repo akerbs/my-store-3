e805563f4c6f2152154e019a1c03fae7c28015f8
Anatol Kerbs <anker2702@gmail.com>
2020-10-30 09:52:16 +0300

    301020_5

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -16,25 +16,27 @@ import Hidden from "@material-ui/core/Hidden"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import PropTypes from "prop-types"
 import CheckoutCartOverview from "../components/Checkout/CheckoutCartOverview"
+import Grid from "@material-ui/core/Grid"
+
 import { Elements } from "@stripe/react-stripe-js"
 import getStripe from "../utils/stripejs"
 
 const window = require("global/window")
 const boxMinHeight = window.innerWidth <= 599 ? "50vw" : "100vh"
-const boxWidth = window.innerWidth <= 599 ? "100vw" : "50vw"
+// const boxWidth = window.innerWidth <= 599 ? "100vw" : "50vw"
 
 const useStyles = makeStyles(theme => ({
   contentWrapper: {
     margin: 0,
     padding: 0,
-    width: "100vw",
+    // width: "100vw",
     minHeight: "100vh",
     // overflowY: "scroll",
   },
   boxLeft: {
     boxShadow: " 1px 0 3px -1px rgba(0, 0, 0, 0.1)",
     minHeight: boxMinHeight,
-    width: boxWidth,
+    // width: boxWidth,
     // minWidth: "50vw",
     // maxWidth: "50vw",
     margin: 0,
@@ -44,7 +46,7 @@ const useStyles = makeStyles(theme => ({
   boxRight: {
     boxShadow: " -1px 0 3px -1px rgba(0, 0, 0, 0.1)",
     minHeight: boxMinHeight,
-    width: boxWidth,
+    // width: boxWidth,
     // minWidth: "50vw",
     // maxWidth: "50vw",
     margin: 0,
@@ -60,106 +62,96 @@ function Checkout() {
     <>
       <Elements stripe={getStripe()}>
         <CssBaseline />
-        {/* <Container className={classes.contentWrapper} id="wrapper"> */}
-
-        <Hidden smDown>
-          <div className={classes.contentWrapper}>
-            <div className={classes.boxLeft}>
-              {/* <CheckoutCartOverview /> */}
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
-              dicta officiis eos quas, id nihil asperiores nisi voluptatum quos
-              tempora. Quibusdam a hic doloremque saepe mollitia ipsum nihil
-              consequatur ullam, sequi officiis, optio illum tempora
-              accusantium! Sit alias praesentium accusamus nobis dignissimos
-              reprehenderit similique nostrum explicabo natus, deserunt quae
-              nesciunt quam laudantium eligendi in quos neque eveniet molestias,
-              quaerat quas provident autem ad exercitationem facere! Atque ipsum
-              cum dolores accusamus recusandae itaque labore a quia eligendi
-              culpa aliquid pariatur exercitationem nihil quo et hic, veniam
-              laudantium laboriosam sapiente veritatis voluptate consequuntur
-              quis! Accusantium libero expedita aliquid eum iste minima officia,
-              adipisci accusamus corrupti temporibus ea. Quasi maxime quibusdam
-              porro voluptatum quis, deserunt totam blanditiis sit quam sapiente
-              iusto placeat ullam saepe dicta quod atque impedit praesentium
-              alias non error laborum expedita suscipit. Ipsam consequatur
-              fugiat inventore non, autem tenetur harum quia odit, fugit quis
-              maxime neque minima labore consequuntur ea nemo? In dolorem
-              dignissimos minus repudiandae maxime minima nobis reprehenderit
-              officiis debitis, ducimus magni quisquam vel necessitatibus
-              laudantium earum, quasi consequatur sunt obcaecati asperiores
-              beatae? Voluptate excepturi rerum obcaecati? Totam dolorum
-              molestiae dolores earum dolore voluptatum accusantium ut ullam
-              saepe eligendi sunt omnis iure cupiditate aperiam, nihil quos,
-              laboriosam velit libero! Beatae sapiente laborum mollitia eos
-              doloremque aspernatur ex fugit ratione, voluptatibus distinctio
-              maxime debitis maiores natus accusantium omnis recusandae ullam
-              deserunt illo culpa est expedita vel quas? Iusto obcaecati quia
-              facilis consequatur, voluptate totam vel id eius debitis amet
-              consectetur quas cumque nostrum recusandae praesentium. Provident
-              architecto ea tenetur quo rerum non, numquam animi corrupti
-              laborum quod laudantium beatae quis, placeat totam in quia. Fugiat
-              minus voluptate ut beatae animi? Vel quam dolorum perferendis
-              ratione, consectetur dolore repellat ducimus! Consectetur, eum
-              ducimus! Illo eius placeat provident culpa quae eaque voluptates
-              amet, libero odit fuga facilis neque omnis, numquam
-              necessitatibus.
-            </div>
-
-            <div className={classes.boxRight}>
-              {/* <SplitForm /> */}
-              {/* <MyCheckoutForm /> */}
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
-              dicta officiis eos quas, id nihil asperiores nisi voluptatum quos
-              tempora. Quibusdam a hic doloremque saepe mollitia ipsum nihil
-              consequatur ullam, sequi officiis, optio illum tempora
-              accusantium! Sit alias praesentium accusamus nobis dignissimos
-              reprehenderit similique nostrum explicabo natus, deserunt quae
-              nesciunt quam laudantium eligendi in quos neque eveniet molestias,
-              quaerat quas provident autem ad exercitationem facere! Atque ipsum
-              cum dolores accusamus recusandae itaque labore a quia eligendi
-              culpa aliquid pariatur exercitationem nihil quo et hic, veniam
-              laudantium laboriosam sapiente veritatis voluptate consequuntur
-              quis! Accusantium libero expedita aliquid eum iste minima officia,
-              adipisci accusamus corrupti temporibus ea. Quasi maxime quibusdam
-              porro voluptatum quis, deserunt totam blanditiis sit quam sapiente
-              iusto placeat ullam saepe dicta quod atque impedit praesentium
-              alias non error laborum expedita suscipit. Ipsam consequatur
-              fugiat inventore non, autem tenetur harum quia odit, fugit quis
-              maxime neque minima labore consequuntur ea nemo? In dolorem
-              dignissimos minus repudiandae maxime minima nobis reprehenderit
-              officiis debitis, ducimus magni quisquam vel necessitatibus
-              laudantium earum, quasi consequatur sunt obcaecati asperiores
-              beatae? Voluptate excepturi rerum obcaecati? Totam dolorum
-              molestiae dolores earum dolore voluptatum accusantium ut ullam
-              saepe eligendi sunt omnis iure cupiditate aperiam, nihil quos,
-              laboriosam velit libero! Beatae sapiente laborum mollitia eos
-              doloremque aspernatur ex fugit ratione, voluptatibus distinctio
-              maxime debitis maiores natus accusantium omnis recusandae ullam
-              deserunt illo culpa est expedita vel quas? Iusto obcaecati quia
-              facilis consequatur, voluptate totam vel id eius debitis amet
-              consectetur quas cumque nostrum recusandae praesentium. Provident
-              architecto ea tenetur quo rerum non, numquam animi corrupti
-              laborum quod laudantium beatae quis, placeat totam in quia. Fugiat
-              minus voluptate ut beatae animi? Vel quam dolorum perferendis
-              ratione, consectetur dolore repellat ducimus! Consectetur, eum
-              ducimus! Illo eius placeat provident culpa quae eaque voluptates
-              amet, libero odit fuga facilis neque omnis, numquam
-              necessitatibus.
-            </div>
-          </div>
-        </Hidden>
-        {/* Middle up hide - but show for little viewport */}
-        <Hidden mdUp>
-          <CheckoutCartOverview />
-          <MyCheckoutForm />
-        </Hidden>
-        {/* </Container> */}
+        <Grid container spacing={0} className={classes.contentWrapper}>
+          <Grid item xs={12} sm={6} className={classes.boxLeft}>
+            {/* <div className={classes.contentWrapper}>
+            <div className={classes.boxLeft}> */}
+            {/* <CheckoutCartOverview /> */}
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
+            dicta officiis eos quas, id nihil asperiores nisi voluptatum quos
+            tempora. Quibusdam a hic doloremque saepe mollitia ipsum nihil
+            consequatur ullam, sequi officiis, optio illum tempora accusantium!
+            Sit alias praesentium accusamus nobis dignissimos reprehenderit
+            similique nostrum explicabo natus, deserunt quae nesciunt quam
+            laudantium eligendi in quos neque eveniet molestias, quaerat quas
+            provident autem ad exercitationem facere! Atque ipsum cum dolores
+            accusamus recusandae itaque labore a quia eligendi culpa aliquid
+            pariatur exercitationem nihil quo et hic, veniam laudantium
+            laboriosam sapiente veritatis voluptate consequuntur quis!
+            Accusantium libero expedita aliquid eum iste minima officia,
+            adipisci accusamus corrupti temporibus ea. Quasi maxime quibusdam
+            porro voluptatum quis, deserunt totam blanditiis sit quam sapiente
+            iusto placeat ullam saepe dicta quod atque impedit praesentium alias
+            non error laborum expedita suscipit. Ipsam consequatur fugiat
+            inventore non, autem tenetur harum quia odit, fugit quis maxime
+            neque minima labore consequuntur ea nemo? In dolorem dignissimos
+            minus repudiandae maxime minima nobis reprehenderit officiis
+            debitis, ducimus magni quisquam vel necessitatibus laudantium earum,
+            quasi consequatur sunt obcaecati asperiores beatae? Voluptate
+            excepturi rerum obcaecati? Totam dolorum molestiae dolores earum
+            dolore voluptatum accusantium ut ullam saepe eligendi sunt omnis
+            iure cupiditate aperiam, nihil quos, laboriosam velit libero! Beatae
+            sapiente laborum mollitia eos doloremque aspernatur ex fugit
+            ratione, voluptatibus distinctio maxime debitis maiores natus
+            accusantium omnis recusandae ullam deserunt illo culpa est expedita
+            vel quas? Iusto obcaecati quia facilis consequatur, voluptate totam
+            vel id eius debitis amet consectetur quas cumque nostrum recusandae
+            praesentium. Provident architecto ea tenetur quo rerum non, numquam
+            animi corrupti laborum quod laudantium beatae quis, placeat totam in
+            quia. Fugiat minus voluptate ut beatae animi? Vel quam dolorum
+            perferendis ratione, consectetur dolore repellat ducimus!
+            Consectetur, eum ducimus! Illo eius placeat provident culpa quae
+            eaque voluptates amet, libero odit fuga facilis neque omnis, numquam
+            necessitatibus.
+            {/* </div> */}
+          </Grid>
+          <Grid item xs={12} sm={6} className={classes.boxRight}>
+            {/* <div className={classes.boxRight}> */}
+            {/* <SplitForm /> */}
+            {/* <MyCheckoutForm /> */}
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
+            dicta officiis eos quas, id nihil asperiores nisi voluptatum quos
+            tempora. Quibusdam a hic doloremque saepe mollitia ipsum nihil
+            consequatur ullam, sequi officiis, optio illum tempora accusantium!
+            Sit alias praesentium accusamus nobis dignissimos reprehenderit
+            similique nostrum explicabo natus, deserunt quae nesciunt quam
+            laudantium eligendi in quos neque eveniet molestias, quaerat quas
+            provident autem ad exercitationem facere! Atque ipsum cum dolores
+            accusamus recusandae itaque labore a quia eligendi culpa aliquid
+            pariatur exercitationem nihil quo et hic, veniam laudantium
+            laboriosam sapiente veritatis voluptate consequuntur quis!
+            Accusantium libero expedita aliquid eum iste minima officia,
+            adipisci accusamus corrupti temporibus ea. Quasi maxime quibusdam
+            porro voluptatum quis, deserunt totam blanditiis sit quam sapiente
+            iusto placeat ullam saepe dicta quod atque impedit praesentium alias
+            non error laborum expedita suscipit. Ipsam consequatur fugiat
+            inventore non, autem tenetur harum quia odit, fugit quis maxime
+            neque minima labore consequuntur ea nemo? In dolorem dignissimos
+            minus repudiandae maxime minima nobis reprehenderit officiis
+            debitis, ducimus magni quisquam vel necessitatibus laudantium earum,
+            quasi consequatur sunt obcaecati asperiores beatae? Voluptate
+            excepturi rerum obcaecati? Totam dolorum molestiae dolores earum
+            dolore voluptatum accusantium ut ullam saepe eligendi sunt omnis
+            iure cupiditate aperiam, nihil quos, laboriosam velit libero! Beatae
+            sapiente laborum mollitia eos doloremque aspernatur ex fugit
+            ratione, voluptatibus distinctio maxime debitis maiores natus
+            accusantium omnis recusandae ullam deserunt illo culpa est expedita
+            vel quas? Iusto obcaecati quia facilis consequatur, voluptate totam
+            vel id eius debitis amet consectetur quas cumque nostrum recusandae
+            praesentium. Provident architecto ea tenetur quo rerum non, numquam
+            animi corrupti laborum quod laudantium beatae quis, placeat totam in
+            quia. Fugiat minus voluptate ut beatae animi? Vel quam dolorum
+            perferendis ratione, consectetur dolore repellat ducimus!
+            Consectetur, eum ducimus! Illo eius placeat provident culpa quae
+            eaque voluptates amet, libero odit fuga facilis neque omnis, numquam
+            necessitatibus.
+            {/* </div> */}
+          </Grid>
+        </Grid>
+        {/* </div> */}
       </Elements>
     </>
   )
 }
 
-Checkout.propTypes = {
-  width: PropTypes.oneOf(["lg", "md", "sm", "xl", "xs"]).isRequired,
-}
-export default withWidth()(Checkout)
+export default Checkout
