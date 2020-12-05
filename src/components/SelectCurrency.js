baacb1395e5c6f7a9c6c5813eeaeb457df5f2b1c
Anatol Kerbs <anker2702@gmail.com>
2020-12-05 16:29:02 +0300

    51220_33

--- src/components/SelectCurrency.js
+++ src/components/SelectCurrency.js
@@ -51,7 +51,7 @@ export default function SelectCurrency() {
             clearCart()
           }}
           // onChange={handleCurrencyChange}
-          style={{ color: "white", maxWidth: "3.3rem" }}
+          style={{ color: "white", maxWidth: "3.4rem" }}
         >
           <MenuItem value={"USD"}>USD</MenuItem>
           <MenuItem value={"EUR"}>EUR</MenuItem>
