3db33e5b4ca39baccbf0cbef787ee245cf38f7b2
Anatol Kerbs <anker2702@gmail.com>
2020-12-18 14:19:38 +0300

    181220_15

--- src/components/AllProductsPage/Items.js
+++ src/components/AllProductsPage/Items.js
@@ -81,7 +81,7 @@ export default function () {
           }
 
           return (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Grid item xs={12} sm={6} md={4} lg={3} id={newSku.linkId}>
               <ItemCard
                 sku={newSku}
                 key={item.productId}
